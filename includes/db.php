d266559900082e5460e51228c4d70c60525f9ebc
gabewebd <176041945+gabewebd@users.noreply.github.com>
2025-03-29 21:41:10 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,14 @@
 <?php
-$host = 'fdb1029.awardspace.net';
-$user = '4611173_wellnessapparel';
-$pass = 'DWEB2025_wellness_apparel';
-$db = '4611173_wellnessapparel';
+$host = 'localhost'; // Your database host
+$user = 'root';      // Your database username
+$pass = '';          // Your database password
+$db = '4611173_wellnessapparel'; // Your database name
+
 $conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
